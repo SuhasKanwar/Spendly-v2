d08ca6d4848d560da1e758b4c1938b20f6cfe069
SuhasKanwar <suhas.kanwar@gmail.com>
2025-02-21 19:47:19 +0530

    Removed the dist config

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  distDir: 'dist',
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
   }
 };
 
